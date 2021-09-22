31dbff80f064a9bd353b77d7ab4182eb40857ce6
liyongjun1 <yogilee@vip.qq.com>
2021-09-22 11:45:28 +0800

    Update MultiRedisConfigIFace.java

--- src/main/java/com/yj/internet/multi/redis/factory/configuration/MultiRedisConfigIFace.java
+++ src/main/java/com/yj/internet/multi/redis/factory/configuration/MultiRedisConfigIFace.java
@@ -4,4 +4,5 @@ package com.yj.internet.multi.redis.factory.configuration;
  * Created by LiYongJun on 2021/9/14 10:09 上午.
  */
 public interface MultiRedisConfigIFace {
+    RedisConnectionFactory redisConnectionFactory(ClientResources clientResources);
 }
