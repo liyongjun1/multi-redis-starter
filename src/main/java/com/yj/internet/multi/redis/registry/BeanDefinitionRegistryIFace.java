5b1b02b3080f9e6c78fc31cd65f46c30746e9bc9
liyongjun1 <yogilee@vip.qq.com>
2021-09-22 15:09:34 +0800

    Update BeanDefinitionRegistryIFace.java

--- src/main/java/com/yj/internet/multi/redis/registry/BeanDefinitionRegistryIFace.java
+++ src/main/java/com/yj/internet/multi/redis/registry/BeanDefinitionRegistryIFace.java
@@ -4,4 +4,9 @@ package com.yj.internet.multi.redis.registry;
  * Created by LiYongJun on 2021/9/14 10:16 上午.
  */
 public interface BeanDefinitionRegistryIFace {
+
+    void registerBeanDefinition(boolean isPrimary, String redisName, List<String> aliasList, P configProperties, MultiRedisClient redisClient);
+
+    BeanDefinitionBuilder createRedisConfigBeanDefinition(P configProperties, MultiRedisClient redisClient);
+
 }
