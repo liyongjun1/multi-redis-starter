53284192a690208b3a9d2598805fffbb5ec5ff06
liyongjun1 <yogilee@vip.qq.com>
2021-09-22 14:29:43 +0800

    Create LettuceClusterRedisConfiguration.java

---
+++ src/main/java/com/yj/internet/multi/redis/factory/lettuce/LettuceClusterRedisConfiguration.java
@@ -0,0 +1,28 @@
+package com.yj.internet.multi.redis.factory.lettuce;
+
+/**
+ * user cluster redis集群
+ *
+ * @Author: LiYongJun
+ * @Date: 2020/9/22 14:27
+ */
+public class LettuceClusterRedisConfiguration extends AbstractLettuceConfiguration {
+    public static final String CONFIG_MEMBER_NAME = "cluster";
+
+    private ClusterRedisConfig cluster;
+
+    public LettuceClusterRedisConfiguration(MultiRedisProperties properties, ObjectProvider<LettuceClientConfigurationBuilderCustomizer> builderCustomizers) {
+        super(properties, builderCustomizers);
+    }
+
+    public RedisConnectionFactory redisConnectionFactory(ClientResources clientResources) {
+        LettuceClientConfiguration clientConfig = getLettuceClientConfiguration(clientResources,
+                this.properties.getBase().getPool(), cluster);
+        return createClusterConnectionFactory(clientConfig, cluster, Optional.empty());
+    }
+
+    public void setCluster(ClusterRedisConfig cluster) {
+        this.cluster = cluster;
+    }
+
+}
