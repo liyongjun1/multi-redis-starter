dfd50d3ce61697d81bd3aa94457db1aba15d36a6
liyongjun1 <yogilee@vip.qq.com>
2021-09-22 11:57:03 +0800

    Create JedisStandaloneRedisConfiguration.java

---
+++ src/main/java/com/yj/internet/multi/redis/factory/jedis/JedisStandaloneRedisConfiguration.java
@@ -0,0 +1,25 @@
+/**
+ * standalone redis
+ *
+ * @Author: LiYongJun
+ * @Date: 2021/9/22 11:55
+ */
+public class JedisStandaloneRedisConfiguration extends AbstractJedisConfiguration {
+
+    public static final String CONFIG_MEMBER_NAME = "standaloneRedisConfig";
+
+    private StandaloneRedisConfig standaloneRedisConfig;
+
+    public JedisStandaloneRedisConfiguration(MultiRedisProperties properties, ObjectProvider<JedisClientConfigurationBuilderCustomizer> builderCustomizers) {
+        super(properties, builderCustomizers);
+    }
+
+    public RedisConnectionFactory redisConnectionFactory(ClientResources clientResources) {
+        JedisClientConfiguration clientConfig = getJedisClientConfiguration(properties.getBase().getPool(), standaloneRedisConfig.getUrl(), standaloneRedisConfig);
+        return createStandaloneConnectionFactory(clientConfig, standaloneRedisConfig);
+    }
+
+    public void setStandaloneRedisConfig(StandaloneRedisConfig standaloneRedisConfig) {
+        this.standaloneRedisConfig = standaloneRedisConfig;
+    }
+}
