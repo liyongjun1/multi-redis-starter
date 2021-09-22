1a7a7bc5acb393d1d35e375c0df0ed49f9f0be46
liyongjun1 <yogilee@vip.qq.com>
2021-09-22 14:59:52 +0800

    Update MultiRedisProperties.java

--- src/main/java/com/yj/internet/multi/redis/properties/MultiRedisProperties.java
+++ src/main/java/com/yj/internet/multi/redis/properties/MultiRedisProperties.java
@@ -8,4 +8,49 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = MultiRedisConstants.MULTI_REDIS_CONFIG_PREFIX)
 public class MultiRedisProperties {
+    private String primary;
+    private MultiRedisClient redisClient = MultiRedisClient.JEDIS;
+    private MultiRedisBaseCfg base;
+    private Map<String, ClusterRedisConfig> cluster;
+    private Map<String, StandaloneRedisConfig> standalone;
+
+    public Map<String, ClusterRedisConfig> getCluster() {
+        return cluster;
+    }
+
+    public void setCluster(Map<String, ClusterRedisConfig> cluster) {
+        this.cluster = cluster;
+    }
+
+    public Map<String, StandaloneRedisConfig> getStandalone() {
+        return standalone;
+    }
+
+    public void setStandalone(Map<String, StandaloneRedisConfig> standalone) {
+        this.standalone = standalone;
+    }
+
+    public String getPrimary() {
+        return primary;
+    }
+
+    public void setPrimary(String primary) {
+        this.primary = primary;
+    }
+
+    public MultiRedisClient getRedisClient() {
+        return redisClient;
+    }
+
+    public void setRedisClient(MultiRedisClient redisClient) {
+        this.redisClient = redisClient;
+    }
+
+    public MultiRedisBaseCfg getBase() {
+        return base;
+    }
+
+    public void setBase(MultiRedisBaseCfg base) {
+        this.base = base;
+    }
 }
